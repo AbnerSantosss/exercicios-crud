c2950aa0339912218ee6763d88d8be33d0f7e8d8
Abner Sasntos <abnersenna@hotmail.com>
2022-09-18 16:29:16 -0300

    ajustado a estrutura do projeto e feito modal para postagens

--- src/main.js
+++ src/main.js
@@ -1,51 +1,52 @@
-let form = document.getElementById("form");
-let input = document.getElementById("input");
-let msg = document.getElementById("msg");
-let posts = document.getElementById("posts");
+let form = document.getElementById('form')
+let input = document.getElementById('input')
+let msg = document.getElementById('msg')
+let posts = document.getElementById('posts')
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("button clicked");
-  formValidation();
-});
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log('button clicked')
+  formValidation()
+})
 
 let formValidation = () => {
-  if (input.value === "") {
-    msg.innerHTML = "Post cannot be blank";
-    console.log("failure");
+  if (input.value === '') {
+    msg.innerHTML = 'A postagem não pode ficar em branco'
+    console.log('failure')
   } else {
-    acceptData();
+    acceptData()
   }
-};
+}
 
-let data = {};
+let data = {}
 
 let acceptData = () => {
-  data["text"] = input.value;
-  createPosts();
-  console.log(data);
-};
+  data['text'] = input.value
+  createPosts()
+  console.log(data)
+}
 
 function createPosts() {
   posts.innerHTML += `
   <div>
     <p>${data.text}</p>
      <span>
-     <i onClick=deletePost(this) class="fas fa-trash-alt"></i>
+     <i onClick=deletePost(this) class="fas fa-trash-alt"></i></span>
+     <span>
      <i onClick=editPost(this) class="fas fa-edit"></i>
      </span>
   </div>
-  `;
-  input.value = "";
+  `
+  input.value = ''
 }
 
 function deletePost(id) {
-  id.parentElement.parentElement.remove();
+  id.parentElement.parentElement.remove()
 }
 
 function editPost(id) {
-  console.log("delete");
-  input.value = id.parentElement.previousElementSibling.innerHTML;
+  console.log('delete')
+  input.value = id.parentElement.previousElementSibling.innerHTML
   console.log(` ${input.value}`)
- id.parentElement.parentElement.remove();
+  id.parentElement.parentElement.remove()
 }
